e3de0af4759df51f385bc0917954a2b2d896ab91
Yooshin Kim <yusinkim.or@gmail.com>
2023-09-02 12:29:47 +0900

    Feat: 바텀시트 커스텀 실패, 기본 버전 사용

--- pages/diary/index.vue
+++ pages/diary/index.vue
@@ -1,16 +1,97 @@
 <template>
     <div class="viewport">
-        <v-icon class="ic_close" />
+        <v-icon class="ic_close" @click="close" />
 
         <img
             class="image"
             src="	https://storage.googleapis.com/docent/f974e3e8-a7ce-4775-9a1a-b770cca7978d.png"
+            @click="open"
         />
+
+        <bottom-sheet ref="myBottomSheet">
+            <h1>Lorem Ipsum</h1>
+            <h2>What is Lorem Ipsum?</h2>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <h1>Lorem Ipsum</h1>
+            <h2>What is Lorem Ipsum?</h2>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <h1>Lorem Ipsum</h1>
+            <h2>What is Lorem Ipsum?</h2>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <h1>Lorem Ipsum</h1>
+            <h2>What is Lorem Ipsum?</h2>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <h1>Lorem Ipsum</h1>
+            <h2>What is Lorem Ipsum?</h2>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <h1>Lorem Ipsum</h1>
+            <h2>What is Lorem Ipsum?</h2>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <h1>Lorem Ipsum</h1>
+            <h2>What is Lorem Ipsum?</h2>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <h1>Lorem Ipsum</h1>
+            <h2>What is Lorem Ipsum?</h2>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <hr />
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <h1>Lorem Ipsum</h1>
+            <h2>What is Lorem Ipsum?</h2>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <h1>Lorem Ipsum</h1>
+            <h2>What is Lorem Ipsum?</h2>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <h1>Lorem Ipsum</h1>
+            <h2>What is Lorem Ipsum?</h2>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <h1>Lorem Ipsum</h1>
+            <h2>What is Lorem Ipsum?</h2>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <h1>Lorem Ipsum</h1>
+            <h2>What is Lorem Ipsum?</h2>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+            <h1>Lorem Ipsum</h1>
+            <h2>What is Lorem Ipsum?</h2>
+            <p><strong>Lorem Ipsum</strong> is simply dummy text</p>
+        </bottom-sheet>
     </div>
 </template>
-<script>
-export default {
-    name: "Diary",
+<script setup>
+// import VueBottomSheet from "@webzlodimir/vue-bottom-sheet";
+// export default {
+//     name: "Diary",
+//     components: { VueBottomSheet },
+//     setup(){
+//         const myBottomSheet = ref(null)
+
+//         return {
+//             myBottomSheet
+//         }
+//     },
+//     methods: {
+//         open() {
+//             myBottomSheet.value.open();
+//         },
+//         close() {
+//             myBottomSheet.value.close();
+//         },
+//     },
+// };
+
+// import VueBottomSheet from "@webzlodimir/vue-bottom-sheet";
+import BottomSheet from "../../components/BottonSheet.vue";
+import { ref } from "vue";
+
+const myBottomSheet = ref(null);
+
+const open = () => {
+    myBottomSheet.value.open();
+};
+
+const close = () => {
+    myBottomSheet.value.close();
 };
 </script>
 <style lang="scss" scoped>
