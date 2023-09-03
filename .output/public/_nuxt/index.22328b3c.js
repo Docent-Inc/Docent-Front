6160d3a1a4ebd98a143ab99f9748d308c546bc64
Yooshin Kim <yusinkim.or@gmail.com>
2023-09-03 20:25:40 +0900

    yarn generate (0903T2023)

--- .output/public/_nuxt/index.22328b3c.js
+++
@@ -1,1 +0,0 @@
-import{l as e,k as t}from"./entry.20988edd.js";const o={class:"viewport"},_={__name:"index",setup(r){return(s,a)=>(t(),e("div",o,"Gallery"))}};export{_ as default};
--- .output/public/_nuxt/index.3147282a.js
+++
@@ -1,1 +0,0 @@
-import{l as e,k as t}from"./entry.20988edd.js";const a={class:"viewport"},n={__name:"index",setup(o){return(r,s)=>(t(),e("div",a,"Calendar"))}};export{n as default};
