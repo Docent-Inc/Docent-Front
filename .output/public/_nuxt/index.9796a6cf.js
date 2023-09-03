8b91d7b33cd73332e72f5e0346a134cc6e362056
Yooshin Kim <yusinkim.or@gmail.com>
2023-09-03 15:48:48 +0900

    yarn generate

---
+++ .output/public/_nuxt/index.9796a6cf.js
@@ -0,0 +1,1 @@
+import{l as e,k as t}from"./entry.38f2c488.js";/* empty css                   */const o={class:"viewport"},n={__name:"index",setup(r){return(s,c)=>(t(),e("div",o,"Report"))}};export{n as default};
--- .output/public/_nuxt/signup.0671b258.js
+++
@@ -1,1 +0,0 @@
-import{a0 as s}from"./entry.351d1f2e.js";const i=s("signup",{state:()=>({user:{nickname:"",age:"",gender:"남",mbti:""},step:0}),actions:{setNickname(e){this.user.nickname=e},setAge(e){this.user.age=e},setGender(e){this.user.gender=e},setMbti(e){this.user.mbti=e},setStep(e){this.step=e>0?e:0}}});export{i as u};
