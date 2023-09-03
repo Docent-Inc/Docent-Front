726cd6294671cff986b5f8b082d8582b9ed553f0
Yooshin Kim <yusinkim.or@gmail.com>
2023-09-03 22:12:54 +0900

    yarn generate (0903T2212) - Fix: meta태그에 뷰포트 관련 설정 추가

--- .output/public/_nuxt/index.7961e3e8.js
+++
@@ -1,1 +0,0 @@
-import{l as e,k as t}from"./entry.99c24b1c.js";const o={class:"viewport"},a={__name:"index",setup(r){return(s,c)=>(t(),e("div",o,"Report"))}};export{a as default};
---
+++ .output/public/_nuxt/index.7e13eba5.js
@@ -0,0 +1,1 @@
+import{l as e,k as t}from"./entry.257fcb0b.js";const o={class:"viewport"},_={__name:"index",setup(r){return(s,a)=>(t(),e("div",o,"Gallery"))}};export{_ as default};
