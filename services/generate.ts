68d8be3967c8dafd0660325e3fd40d206d88adf7
Yooshin Kim <yusinkim.or@gmail.com>
2023-10-29 16:24:56 +0900

    feat(feature/api): axios inteceptor 수정
    - API status에 따라 error로 reject 하는 옵션 해제
    - API 응답 res -> res.data로 수정

--- services/generate.ts
+++ services/generate.ts
@@ -6,30 +6,25 @@ export const useGenerateService = () => {
          * 채팅 리스트 조회
          */
         async getChatList(page: number) {
-            const res = await API.get(`/generate/chat/list?page=${page}`);
-            return res.data;
+            return await API.get(`/generate/chat/list?page=${page}`);
         },
         /**
          * 챗봇 대화
          * @body text
          */
         async generateChat(text: string) {
-            const res = await API.post(`/generate/chat`, {
+            return await API.post(`/generate/chat`, {
                 content: text,
             });
-            return res.data;
         },
         /**
          * 마음 보고서 생성
          */
         async getReport() {
-            const res = await API.get(`/generate/report`);
-            return res.data;
+            return await API.get(`/generate/report`);
         },
         // async getReport() {
-        //     const res = await API.get(`/today/report`);
-        //     console.log(res.data);
-        //     return res.data;
+        //     return await API.get(`/today/report`);
         // },
     };
 };
