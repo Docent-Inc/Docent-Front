b8e001f729611a36a2509617441420451059a2f1
taewan <taewan2002@naver.com>
2023-10-29 23:11:49 +0900

    feat: 설정 페이지 제작

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -17,6 +17,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
         "/gallery",
         "/report",
         "/calendar",
+        "/setting",
     ];
 
     // console.log("****** MIDDLEWARE ******");
