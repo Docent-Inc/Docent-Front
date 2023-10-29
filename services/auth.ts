f8962d0270b6505bad8a0731d690e4bec1ff15b0
taewan <taewan2002@naver.com>
2023-10-29 16:34:57 +0900

    Revert "Revert "feat(feature/api): axios inteceptor 수정""
    This reverts commit 26cdcc38a06f7ed51f62b8ca60a76ed97032cff0.

--- services/auth.ts
+++ services/auth.ts
@@ -18,56 +18,50 @@ export const useAuthService = () => {
          * 카카오 로그인 요청
          */
         async getKakaoLogin() {
-            const res = await API.get(KAKAO_SIGNIN_URL);
-            return res.data;
+            return await API.get(KAKAO_SIGNIN_URL);
         },
         /**
          * 카카오 콜백 요청
          * @params code
          */
         async getKakaoCallback(code: string) {
-            const res = await API.get(KAKAO_SIGNIN_CALLBACK_URL, {
+            return await API.get(KAKAO_SIGNIN_CALLBACK_URL, {
                 params: { code: code },
             });
-            return res.data;
         },
         /**
          * 초기 정보 등록
          * @body user
          */
         async signup(data: signupModel) {
-            const res = await API.post(`/auth/update`, {
+            return await API.post(`/auth/update`, {
                 ...data,
             });
-            return res.data;
         },
         /**
          * 유저 정보 조회
          * @body userId
          */
         async getUserInfo() {
-            const res = await API.get(`/admin/user/info`);
-            return res.data;
+            return await API.get(`/admin/user/info`);
         },
         /**
          * 닉네임 변경 요청
          * @body nickname
          */
         async changeNickname(nickname: string) {
-            const res = await API.post(`/auth/change/nickname`, {
+            return await API.post(`/auth/change/nickname`, {
                 nickname: nickname,
             });
-            return res.data;
         },
         /**
          * MBTI 변경 요청
          * @body mbti
          */
         async changeMBTI(mbti: string) {
-            const res = await API.post(`/auth/change/mbti`, {
+            return await API.post(`/auth/change/mbti`, {
                 mbti: mbti,
             });
-            return res.data;
         },
         /**
          * 액세스 토큰 재발급
