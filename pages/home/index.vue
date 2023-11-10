8ce0ef3cb280962e44d3b436da4c111b86317a38
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-11-11 02:56:31 +0900

    feat(feature/main): 홈화면 구현

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,59 +1,53 @@
 <template>
-    <div class="header">
-        <!-- <v-icon class="logo_docent" /> -->
-    </div>
-    <div class="contents">
-        <div class="title">{{ $dayjs().format("YYYY.MM.DD") }}</div>
-        <div class="main-title">
-            안녕하세요 {{ user?.nickname }}님 <br />오늘의 이벤트예요
+    <header class="header">
+        <div class="ic_logo_box" @click="() => this.$router.push('/home')">
+            <v-icon class="ic_home_logo" />
         </div>
-        <!-- 오늘의 일정 -->
-        <div class="main-calendar main-description">
-            <v-icon class="ic_calendar" /> &nbsp;
-            <div v-if="calendar.length > 0">{{ calendar[0].title }}&nbsp;►</div>
-            <div v-else>일정이 없네요!</div>
+        <div
+            class="ic_setting_box"
+            @click="() => this.$router.push('/setting')"
+        >
+            <v-icon class="ic_setting" />
         </div>
+    </header>
+    <main class="contents">
+        <!-- 홈 문구 -->
+        <Greeting :user="user" :luck="luck" />
 
-        <!-- 오늘의 기록 -->
-        <div class="main-title">오늘의 기록</div>
-        <div class="main-description">
-            오늘 {{ user?.nickname }}님은 이런 그림을 그리셨네요
-        </div>
-        <div class="main-slides">
-            <div
-                class="main-slide"
-                v-if="record.morning"
-                :style="`background-image: url(${record.morning.image_url})`"
-            ></div>
-            <div
-                class="main-slide"
-                v-if="record.night"
-                :style="`background-image: url(${record.night.image_url})`"
-            ></div>
+        <div class="contents-wrapper">
+            <section class="chat">
+                <button
+                    type="button"
+                    class="chat__btn"
+                    @click="() => this.$router.push('/chat')"
+                >
+                    Looki와 대화하러 가기
+                </button>
+            </section>
 
-            <div
-                class="main-slide empty"
-                v-if="!record.morning && !record.night"
-                @click="this.$router.push(`/chat`)"
-            >
-                {{ user?.nickname }}님의 그림을 <br />그려주세요!
-            </div>
-        </div>
+            <!-- 디데이 -->
+            <DDays
+                :calendar="calendar"
+                :isCalendarLoading="isCalendarLoading"
+            />
 
-        <!-- 오늘의 운세 -->
-        <div class="main-title">오늘의 운세</div>
-        <div class="main-description">{{ lucky }}</div>
-    </div>
+            <!-- 오늘의 기록 -->
+            <Records :record="record" />
+        </div>
+    </main>
 </template>
 
 <script>
 import { mapState } from "pinia";
 import { useUserStore } from "~/store/user";
 import { useTodayService } from "../../services/today";
-import { useGenerateService } from "~/services/generate";
+import Greeting from "../../components/home/Greeting.vue";
+import DDays from "../../components/home/DDays.vue";
+import Records from "../../components/home/Records.vue";
 
 export default {
     name: "Home",
+    components: { Greeting, DDays, Records },
     setup() {
         definePageMeta({
             layout: "main",
@@ -62,33 +56,70 @@ export default {
     computed: {
         ...mapState(useUserStore, ["user"]),
     },
+    methods: {},
     data() {
         return {
             calendar: [],
+            isCalendarLoading: true,
             record: {},
-            lucky: "",
+            luck: "",
         };
     },
     mounted() {
-        const { getTodayCalendar, getTodayRecord, getTodayLucky } =
+        const { getTodayLucky, getTodayCalendar, getTodayRecord } =
             useTodayService();
 
+        getTodayLucky().then((res) => {
+            if (res.success) this.luck = res.data.luck;
+        });
+
         getTodayCalendar().then((res) => {
-            if (res.success) this.calendar = res.data;
+            if (res.success) {
+                this.calendar = res.data;
+                this.isCalendarLoading = false;
+            }
         });
 
         getTodayRecord().then((res) => {
             if (res.success) this.record = res.data;
-        });
-
-        getTodayLucky().then((res) => {
-            if (res.success) this.lucky = res.data.luck;
         });
     },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "@/assets/scss/valuables.scss";
+@import "@/assets/scss/mixins.scss";
+
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem;
+
+    .ic_logo_box {
+        height: 29px;
+        width: 62px;
+        cursor: pointer;
+
+        > i {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+    }
+    .ic_setting_box {
+        width: 32px;
+        height: 32px;
+        cursor: pointer;
+
+        > i {
+            width: 100%;
+            height: 100%;
+        }
+    }
+}
+
 .contents {
     height: calc(100% - (60px));
     height: calc(100% - (60px + constant(safe-area-inset-top)));
@@ -97,82 +128,48 @@ export default {
     margin-top: 60px;
     margin-top: calc(60px + constant(safe-area-inset-top));
     margin-top: calc(60px + env(safe-area-inset-top));
+    padding: 0;
 
-    padding: 2rem 0 4rem 2rem;
-}
-
-.chat {
-    width: 80%;
-    margin: 0 auto;
-    padding: 10px 0;
-    border-radius: 40px;
-    position: relative;
-
-    text-align: center;
-    background: #efefef;
-
-    .v-icon {
-        position: absolute;
-        right: 0;
-        margin-right: 15px;
-    }
-}
-
-// .logo_docent {
-//     font-size: 42px;
-//     margin: 0 auto;
-// }
-
-.title {
-    margin-bottom: 1.7rem;
-}
-
-.main-title {
-    margin: 1rem 0;
-}
-
-.main-description {
-    margin-right: 2rem;
-}
-
-.main-calendar {
-    display: flex;
-    margin-bottom: 5rem;
-}
-
-.main-slides {
-    width: 100%;
-    margin-top: 2rem;
-    margin-bottom: 2.7rem;
-    padding-bottom: 2rem;
-
-    display: flex;
-    overflow-x: scroll;
-
-    scrollbar-width: none; /* Firefox */
-    -ms-overflow-style: none; /* IE/Edge */
+    overflow-x: none;
+    scrollbar-width: none;
+    -ms-overflow-style: none;
     &::-webkit-scrollbar {
         display: none;
     }
 
-    .main-slide {
-        flex: 0 0 60%;
-        aspect-ratio: 1/1;
-        border-radius: 0.625rem;
-        margin-right: 1.5rem;
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: cover;
+    .contents-wrapper {
+        padding: 2rem 2rem 0 2rem;
+        background-color: var(--vc-white);
+        margin-top: -2rem;
+        border-radius: 20px;
+        width: 100%;
     }
+}
 
-    .main-slide.empty {
-        background-color: rgba(206, 218, 200, 0.7);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: rgba(77, 77, 77, 0.72);
-        font-family: "Pretendard";
-        text-align: center;
+.chat {
+    width: 100%;
+    margin-top: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &__btn {
+        width: 100%;
+        height: 48px;
+        background: $vc-accent;
+        color: white;
+        border-radius: 12px;
+        font-weight: 600;
+        font-size: var(--vc-text-base);
+        box-shadow: 0px 8px 30px rgba(70, 96, 250, 0.46);
+        font-family: "Pretendard Bold";
     }
+}
+
+.home-skeleton {
+    height: 100%;
+    width: 100%;
+    background: transparent;
+    position: absolute;
 }
 </style>
