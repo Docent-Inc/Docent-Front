8aaa16ed37a22948181f057037c02f4952d553ac
Yooshin Kim <yusinkim.or@gmail.com>
2023-12-20 23:59:51 +0900

    feat(feature/applelogin): 애플로그인 성공 로직 추가

--- pages/callback/apple.vue
+++ pages/callback/apple.vue
@@ -22,6 +22,44 @@ console.log(router);
 onMounted(async () => {
     const res = await getSocialCallback("apple", route.query.code);
     console.log(">> apple res", res);
+
+    if (res.success) {
+        // 로그인 성공 시, 쿠키/store 세팅
+        const { setAccessToken, setRefreshToken, setUser } = useUserStore();
+        const now = new Date();
+        const accessTokenExpires = new Date(
+            now.getTime() + res.data.expires_in * 1000,
+        );
+        const refreshTokenExpires = new Date(
+            now.getTime() + res.data.refresh_expires_in * 1000,
+        );
+
+        useCookie("access_token", {
+            expires: accessTokenExpires,
+        }).value = res.data.access_token;
+        useCookie("expires_in", {
+            expires: accessTokenExpires,
+        }).value = String(accessTokenExpires);
+        useCookie("refresh_token", {
+            expires: refreshTokenExpires,
+        }).value = res.data.refresh_token;
+
+        setAccessToken(res.data.access_token);
+        setRefreshToken(res.data.refresh_token);
+
+        if (res.data.is_signup) {
+            router.push(`/profile/starter`);
+            return;
+        }
+
+        await setUser();
+
+        router.push(`/chat`); // 231216 - v2 /home -> /chat으로 진입점 변경
+    } else {
+        const { reset } = useUserStore();
+        reset();
+        alert(res.message);
+    }
 });
 </script>
 
