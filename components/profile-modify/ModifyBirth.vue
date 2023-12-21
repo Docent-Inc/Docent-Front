d7fe68852e5a9b2149b15ca07212ecbf2905ded9
Doyu-Lee <doyu.lee.0406@gmail.com>
2023-12-21 20:50:02 +0900

    fix(develop): 스크롤 범위 확장

--- components/profile-modify/ModifyBirth.vue
+++ components/profile-modify/ModifyBirth.vue
@@ -156,20 +156,22 @@ export default {
             ];
         },
         handleDragStart(event) {
-            event.stopPropagation();
+            // event.stopPropagation();
+            event.preventDefault();
             this.startY = event.touches[0].clientY;
             this.startX = event.touches[0].clientX;
             this.touchData.lastY = this.startY;
             this.touchData.lastTime = Date.now();
         },
         handleDragMove(event) {
-            event.stopPropagation();
+            // event.stopPropagation();
+            event.preventDefault();
             const currentX = event.touches[0].clientX;
             const currentY = event.touches[0].clientY;
             let deltaY = currentY - this.touchData.lastY;
             const sensitivity = 0.05;
             deltaY *= sensitivity;
- 
+
             const deltaTime = Date.now() - this.touchData.lastTime;
             this.touchData.velocity = deltaY / deltaTime;
             this.touchData.lastY = currentY;
@@ -183,7 +185,7 @@ export default {
             }
         },
         handleDragEnd(event) {
-            event.stopPropagation();
+            // event.stopPropagation();
             this.lastX = event.changedTouches[0].clientX;
             const inertiaDuration = 100;
             const distance = this.touchData.velocity * inertiaDuration;
@@ -261,14 +263,13 @@ export default {
     color: var(--gray-700, #374151);
     font-family: "Pretendard Medium";
     font-size: 16px;
-    line-height: 160%;
+    /* line-height: 160%; */
 }
 .modify-birth-content {
     width: 100%;
     // height: 144px;
     display: flex;
     justify-content: space-around;
-    align-items: center;
 }
 .year-scroller,
 .month-scroller,
@@ -277,6 +278,8 @@ export default {
     // width: 80px;
     flex-direction: column;
     border-radius: 8px;
+    align-items: center;
+    padding-top: 1.3rem;
 }
 
 .year,
