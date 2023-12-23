2ff7d7aa945b408ab56971dd19af0ff2db891aec
taewan <taewan2002@naver.com>
2023-12-23 12:10:40 +0900

    feat(event): 이벤트 페이지 제작

---
+++ pages/event/[id].vue
@@ -0,0 +1,705 @@
+<template>
+  <section class="viewport" :style="dynamicBackground">
+    <header class="header">
+
+      <div class="btn_url" @click="shareURL">
+        <Icon class="ic_url" />
+        <span>URL 공유하기</span>
+      </div>
+    </header>
+
+    <article class="contents" :class="{'contents-shared': shared}">
+      <div class="contents-header">
+        <span class="contents-header-title">
+            <span lang="ko">Looi의 꿈 해석 하러가기&nbsp;</span>
+            <span lang="en">https://look-i.app</span>&nbsp;검색
+        </span>
+      </div>
+<!--      <v-icon class="ic_event_header" />-->
+      <!-- 1. 상단 영역 (날짜, 제목) -->
+      <div class="diary-title-box">
+        <div v-if="isLoading">
+          <div class="diary-date__skeleton"></div>
+          <div class="diary-title__skeleton"></div>
+        </div>
+        <div v-else>
+          <div class="diary-date">
+            {{ $dayjs(diary.create_date).format("YYYY.MM.DD") }}
+          </div>
+
+          <div class="diary-title">
+            <input
+                class="edit-input"
+                ref="inputRef"
+                v-if="isEditMode && editType === 'title'"
+                v-model="diaryTitle"
+                @blur="handleBlur('title')"
+                :style="dynamicInputTop"
+            />
+            <div v-else>{{ diaryTitle || diary.diary_name }}</div>
+          </div>
+
+          <div class="tag-wrap">
+            <div class="tag accent" v-for="tag in diary.keyword">
+              {{ tag }}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- 2. 중간 영역 (이미지, 삭제 버튼) -->
+      <!-- maxWidth="calc((100vh - (60px + 20px)) * 0.6)" -->
+      <Image
+          class="diary-image"
+          :url="diary.image_url"
+          width="calc(100% - 40px)"
+          maxWidth="400px"
+      />
+      <div
+          class="diary-delete"
+          @click="onDelete"
+          :style="bottomTextColor"
+      >
+      </div>
+
+      <!-- 3. 바텀시트 영역 -->
+      <div class="bottom-container" :style="bottomTextColor">
+        <div class="bottom-diary">
+          <div class="bottom-diary-content">
+            <div class="bottom-diary-content-title">
+              <Icon class="ic_memo" />
+              <span v-if="type === '1'">꿈 내용</span>
+              <span v-else>일기 내용</span>
+            </div>
+            <div
+                class="bottom-diary-content-desc"
+            >
+                            <textarea
+                                class="edit-input content"
+                                ref="inputRef"
+                                v-if="isEditMode && editType === 'content'"
+                                v-model="diaryContent"
+                                @blur="handleBlur('content')"
+                                :style="dynamicInputBottom"
+                            />
+              <LimitedLength
+                  :content="diaryContent"
+                  :limitedContentLength="1000"
+                  :diary="diary"
+                  v-if="isEditMode && editType === 'content'"
+                  @update:content="diaryContent = $event"
+              />
+              <div
+                  v-else-if="diaryContent || diary.content"
+                  v-html="
+                                    diaryContent
+                                        ? escapeHtml(diaryContent).replace(
+                                              /\n/g,
+                                              '<br>',
+                                          )
+                                        : escapeHtml(diary.content).replace(
+                                              /\n/g,
+                                              '<br>',
+                                          )
+                                "
+              ></div>
+            </div>
+          </div>
+
+          <div v-if="type == 1" class="bottom-diary-content">
+            <div class="bottom-diary-content-title">
+              <Icon class="ic_crystal" />꿈을 통해 본
+              {{ user?.nickname }}님의 마음
+            </div>
+
+            <div class="bottom-diary-content-desc">
+              {{ diary.resolution }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+
+    <!-- 토스트 -->
+    <Toast
+        v-if="isVisible"
+        @click="isVisible = false"
+        :text="successMessage"
+        :top="60"
+    />
+  </section>
+  <div v-if="shared === 'true'" class="bottom-blur">
+    <v-icon class="ic_start" @click="start"/>
+  </div>
+  <div v-if="shared === 'false'" class="bottom-blur">
+    <v-icon class="ic_event_down_1" @click="goService"/>
+    <v-icon class="ic_event_down_2" @click="goService"/>
+  </div>
+</template>
+<script>
+import { mapState, mapActions } from "pinia";
+import { useUserStore } from "~/store/user";
+import { useRecordStore } from "~/store/record";
+import { escapeHtml } from "~/utils/utils";
+
+import { useDiaryService } from "../../services/diary";
+import Button from "~/components/common/Button.vue";
+import Icon from "~/components/common/Icon.vue";
+import Image from "~/components/common/Image.vue";
+import Toast from "~/components/common/Toast.vue";
+import LimitedLength from "~/components/common/LimitedLength.vue";
+
+export default {
+  name: "event",
+  components: {
+    Button,
+    Icon,
+    Image,
+    Toast,
+    LimitedLength,
+  },
+  data() {
+    return {
+      diary: {},
+      type: "1",
+      isLoading: true,
+      isOpen: false,
+      // 수정
+      isEditMode: false,
+      editType: "",
+      diaryTitle: "",
+      diaryContent: "",
+      // 토스트
+      isVisible: false,
+      successMessage: "",
+      shared: true,
+    };
+  },
+  setup() {
+    const { getMorningdiary } = useDiaryService();
+    const { params, query } = getCurrentInstance().proxy.$route;
+    const record = useAsyncData(`content-${params.id}`, async () => {
+      query.type = "1";
+      const res = await getMorningdiary(params.id);
+      if (res.success) {
+        return res.data?.diary || null;
+      } else {
+        return null;
+      }
+    });
+
+    useSeoMeta({
+      title: () => record.data.value?.diary_name,
+      ogTitle: () => record.data.value?.diary_name,
+      ogDescription: () => record.data.value?.content,
+    });
+  },
+  computed: {
+    ...mapState(useUserStore, ["user"]),
+    dynamicBackground() {
+      let background_color = `rgb(0, 0, 0)`;
+      let text_color = "#fff";
+
+      if (this.diary.background_color) {
+        const colorList = JSON.parse(this.diary.background_color);
+
+        if (colorList.length > 1) {
+          background_color = `linear-gradient(rgb${colorList[0]}, rgb${colorList[1]})`;
+          text_color = getTextColorForBackground([
+            `rgb${colorList[0]}`,
+          ]);
+        } else {
+          background_color = `rgb${colorList[0]}`;
+          text_color = getTextColorForBackground([
+            `rgb${colorList[0]}`,
+          ]);
+        }
+      }
+
+      return {
+        background: background_color,
+        color: text_color,
+      };
+    },
+    bottomTextColor() {
+      let text_color = "#fff";
+
+      if (this.diary.background_color) {
+        const colorList = JSON.parse(this.diary.background_color);
+
+        if (colorList.length > 1) {
+          text_color = getTextColorForBackground([
+            `rgb${colorList[1]}`,
+          ]);
+        } else {
+          text_color = getTextColorForBackground([
+            `rgb${colorList[0]}`,
+          ]);
+        }
+      }
+
+      return {
+        color: text_color,
+      };
+    },
+    dynamicInputTop() {
+      let text_color = "#fff";
+
+      if (this.diary.background_color) {
+        const colorList = JSON.parse(this.diary.background_color);
+
+        if (colorList.length > 1) {
+          text_color = getTextColorForBackground([
+            `rgb${colorList[0]}`,
+          ]);
+        } else {
+          text_color = getTextColorForBackground([
+            `rgb${colorList[0]}`,
+          ]);
+        }
+      }
+
+      return {
+        color: text_color,
+      };
+    },
+    dynamicInputBottom() {
+      let text_color = "#fff";
+
+      if (this.diary.background_color) {
+        const colorList = JSON.parse(this.diary.background_color);
+
+        if (colorList.length > 1) {
+          text_color = getTextColorForBackground([
+            `rgb${colorList[1]}`,
+          ]);
+        } else {
+          text_color = getTextColorForBackground([
+            `rgb${colorList[0]}`,
+          ]);
+        }
+      }
+
+      return {
+        color: text_color,
+      };
+    },
+    bottomSheetTitle() {
+      let type = this.type === "1" ? "꿈 해석" : "일기";
+      if (this.type === "2" && !this.isOpen) type = "일기 자세히"; // 일기의 경우, '자세히' 보기
+
+      const open = !this.isOpen ? "보기" : "닫기";
+
+      return `${type} ${open}`;
+    },
+  },
+  async mounted() {
+    const { setAccessToken  } = useUserStore();
+    setAccessToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0d2NobzAyMDVAZ21haWwuY29tIiwiZXhwIjoxNDMwMDk4MDQ3Mn0._7CobpeILug91YsayMUsGONO2oddYYpUIpL7hNSuw08");
+
+    const { getMorningdiary } = useDiaryService();
+
+    const id = this.$route.params.id;
+    this.shared = this.$route.query.shared;
+    console.log('shared value:', this.shared);
+    const type = "1";
+
+    const res =
+        type === "1" ? await getMorningdiary(id) : await getNightdiary(id);
+
+    if (!res.success) {
+      this.$eventBus.$emit("onConfirmModal", {
+        title: "조회 실패하였습니다.",
+        desc: res.message,
+        callback: () => {
+          this.$router.back();
+        },
+      });
+    }
+
+    this.diary = res.data.diary;
+
+    if (type == 1) this.diary.keyword = JSON.parse(this.diary.main_keyword);
+    this.isLoading = false;
+  },
+  methods: {
+    ...mapActions(useRecordStore, ["deleteOptimisticRecord"]),
+
+    onDelete() {
+      this.$eventBus.$emit("onCustomModal", {
+        title: "정말 이 기록을 삭제하시겠어요?",
+        desc: "삭제한 기록은 영영 돌아오지 않아요!",
+        confirm: "삭제하기",
+        cancel: "남겨두기",
+        callback: this.deleteDiary,
+      });
+    },
+    async deleteDiary() {
+      const { deleteMorningdiary, deleteNightdiary } = useDiaryService();
+
+      const res =
+          this.type === "1"
+              ? await deleteMorningdiary(this.diary.id)
+              : await deleteNightdiary(this.diary.id);
+
+      if (res.success) {
+        // 성공 시, 리스트 페이지로 이동
+        this.$eventBus.$emit("onConfirmModal", {
+          title: "삭제되었습니다.",
+          callback: () => {
+            this.$router.back();
+          },
+        });
+        this.deleteOptimisticRecord(this.diary);
+      } else {
+        // 실패 시, 문구 띄우고 새로고침
+        this.$eventBus.$emit("onConfirmModal", {
+          title: "삭제에 실패하였습니다.",
+          desc: res.message,
+        });
+      }
+    },
+    async start() {
+      this.$router.push(
+          `/event`,
+      )
+    },
+    async goService() {
+      const url = "https://pf.kakao.com/_vNxnRG/103064091";
+      window.open(url, "_blank");
+    },
+    async shareURL() {
+      const url = window.location.href;
+      // "https://docent.zip/share/${this.diary.id}?type=${this.type}"
+
+      try {
+        if (!navigator?.clipboard?.writeText)
+          throw new Error(
+              "복사 기능이 제공되지 않는 브라우저입니다.",
+          );
+
+        // 클립보드에 복사
+        window.navigator.clipboard
+            .writeText(url.replace("false", "true"))
+            .then(() => {
+              this.$eventBus.$emit("onConfirmModal", {
+                title: "URL이 복사되었습니다.",
+              });
+            });
+      } catch (e) {
+        console.error(e);
+        this.$eventBus.$emit("onConfirmModal", {
+          title: "URL 복사에 실패하였습니다",
+          desc: e.message,
+        });
+      }
+    },
+    handleEditMode(type) {
+      this.isEditMode = true;
+      this.editType = type;
+      this.$nextTick(() => {
+        this.$refs.inputRef.focus(); // input 요소에 직접 포커스를 줍니다.
+      });
+
+      if (type === "title") {
+        this.diaryTitle = this.diary.diary_name || ""; // diary_name이 존재하면 기존 값으로 초기화
+      } else if (type === "content") {
+        this.diaryContent = this.diary.content || ""; // content가 존재하면 기존 값으로 초기화
+      }
+    },
+    async handleBlur(type) {
+      this.isEditMode = false;
+
+      if (type === "title" && this.diary.diary_name !== this.diaryTitle) {
+        // 변경된 title 값을 저장하는 로직 추가
+        this.diary.diary_name = this.diaryTitle;
+        this.handleUpdate(
+            this.type,
+            { diary_name: this.diaryTitle },
+            "제목",
+        );
+      } else if (
+          type === "content" &&
+          this.diary.content !== this.diaryContent
+      ) {
+        // 변경된 content 값을 저장하는 로직 추가
+        this.diary.content = this.diaryContent;
+        this.handleUpdate(
+            this.type,
+            { content: this.diaryContent },
+            "내용",
+        );
+      }
+    },
+    async handleUpdate(type, props, updatedPropName) {
+      const { putMorningDiary, putNightDiary } = useDiaryService();
+      let res;
+      if (type === "1") {
+        res = await putMorningDiary(props, this.diary.id);
+      } else if (type === "2") {
+        res = await putNightDiary(props, this.diary.id);
+      }
+
+      if (res.success) {
+        this.successMessage = `${
+            type === "1" ? "꿈" : "일기"
+        } ${updatedPropName} 수정이 완료되었어요! `;
+        this.isVisible = true;
+      }
+
+      // 추후 실행취소 기능 필요
+      setTimeout(() => {
+        this.isVisible = false;
+      }, 2500);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "@/assets/scss/mixins.scss";
+.viewport {
+  // "(138, 137, 140)"
+}
+
+.header {
+  background: none;
+  border: none;
+  justify-content: right;
+
+  padding: 0 20px;
+  // margin-top: 10%;
+
+  .btn_url {
+    right: 0;
+    border-radius: 8px;
+    background: rgba(0, 0, 0, 0.1);
+    padding: 6px 12px 6px 8px;
+
+    /* c1/c1_reg_12 */
+    color: var(--white, #fff);
+    font-family: "Pretendard";
+    font-size: 12px;
+    line-height: 160%; /* 19.2px */
+
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    text-align: center;
+
+    cursor: pointer;
+  }
+}
+.contents {
+  // BottomSheet 높이: 108px =  calc(32px + (12px * 1.5) + 4px) + 40px + 14px;
+  height: calc(100% - (164px));
+  height: calc(100% - (164px + constant(safe-area-inset-top)));
+  height: calc(100% - (164px + env(safe-area-inset-top)));
+  padding: 2rem 0;
+
+  margin-top: calc(60px);
+  margin-top: calc(60px + constant(safe-area-inset-top));
+  margin-top: calc(60px + env(safe-area-inset-top));
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  // justify-content: safe center; // safe를 넣지 않으면 상단이 잘리는 문제 발생, 아이폰에서 잘리는 문제 발생으로 주석처리
+  overflow-x: hidden;
+  gap: 2rem;
+}
+.diary-image {
+  border-radius: 0.94rem;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+
+.diary-title-box {
+  width: calc(100% - 40px);
+  overflow: visible;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+
+  justify-self: center;
+
+  .tag-wrap {
+    margin-bottom: 1rem;
+    /* border: 1px solid red; */
+    flex-wrap: wrap;
+  }
+}
+
+.diary-title {
+  width: 100%;
+
+  /* h1/h1_bold_24 */
+  font-family: "Pretendard Bold";
+  // font-size: 20px;
+  font-size: 1.8rem;
+  line-height: 150%; /* 36px */
+
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: normal;
+
+  &__skeleton {
+    @include skeleton;
+    width: 80%;
+    height: calc(20px * 1.5);
+    margin-top: 5px;
+  }
+}
+
+.diary-date {
+  /* b2/b2_med_14 */
+  font-family: "Pretendard Medium";
+  font-size: 14px;
+  line-height: 160%; /* 22.4px */
+
+  &__skeleton {
+    @include skeleton;
+    width: 20%;
+    height: calc(14px * 1.5);
+  }
+}
+
+.diary-delete {
+  /* c1/c1_reg_12 */
+  font-family: "Pretendard";
+  font-size: 12px;
+  line-height: 160%; /* 19.2px */
+
+  display: flex;
+  gap: 4px;
+  align-items: center;
+
+  align-self: flex-start;
+  margin: 0 20px;
+
+  /* z-index: 2; // 바텀 시트 때문에 z-index 추가 */
+}
+
+.bottom-diary {
+  margin-bottom: 60px;
+  padding: 0 2rem;
+  .bottom-diary-title-box {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+  }
+  .bottom-diary-content {
+    margin: 2rem 0 3rem 0;
+
+    .bottom-diary-content-title {
+      /* color: var(--white, #fff); */
+
+      /* b1/b1_bold_16 */
+      font-family: "Pretendard Bold";
+      font-size: 16px;
+      line-height: 160%; /* 25.6px */
+
+      display: flex;
+      gap: 12px;
+
+      height: auto;
+    }
+
+    .bottom-diary-content-desc {
+      /* color: var(--white, #fff); */
+
+      /* b2/b2_reg_14 */
+      font-family: "Pretendard";
+      font-size: 14px;
+      line-height: 160%; /* 22.4px */
+
+      margin-top: 8px;
+    }
+  }
+}
+
+.edit-input {
+  margin-top: 0.4rem;
+  border: 2.5px solid #ffffff35;
+  background-color: #ffffff15;
+  color: #1f2937;
+  width: 100%;
+  height: auto;
+  border-radius: $border-radius-default;
+  padding: 0.4rem 0.6rem;
+
+  &.content {
+    height: 20rem;
+  }
+}
+
+textarea:focus {
+  border: none;
+  outline: 2.5px solid #ffffff35;
+}
+.bottom-container {
+  width: 100%;
+}
+.bottom-extend {
+  margin-top: 60px;
+  width: 100%;
+}
+.bottom-blur {
+  width: 100%;
+  height: 164px;
+  flex-shrink: 0;
+  background: linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.31) 46.47%, rgba(0, 0, 0, 0.49) 100%);
+  backdrop-filter: blur(2px);
+  position: absolute;
+  bottom: 0; /* 화면 하단에 위치시키기 */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.ic_event_header {
+
+  object-fit: contain;
+  flex-shrink: 0;
+}
+.ic_start {
+  width: 350px;
+  height: 48px;
+  flex-shrink: 0;
+  position: absolute;
+  bottom: 24px;
+}
+.ic_event_down_1 {
+  width: 100%;
+  height: 48px;
+  flex-shrink: 0;
+}
+.ic_event_down_2 {
+  margin-top: 12px;
+  width: 100%;
+  height: 48px;
+  flex-shrink: 0;
+}
+.contents-header {
+  width: 500px;
+  height: 38px;
+  flex-shrink: 0;
+  object-fit: contain;
+  background: var(--indigo-100, #E0E7FF);
+  align-items: center;
+}
+.contents-header-title {
+
+  justify-content: center;
+  margin-top: 8px;
+  display: flex;
+  color: var(--indigo-600, #4F46E5);
+  /* b2/b2_med_14 */
+  font-family: "Pretendard Medium";
+  font-size: 14px;
+}
+.contents-header-title span[lang="en"] {
+  font-family: "Pretendard Bold";
+}
+</style>
---
+++ pages/event/index.vue
@@ -0,0 +1,275 @@
+<template>
+  <div class="header">
+    <div class="header-title-box">
+      <span class="header-title">Looi의 꿈 해석 체험하기</span>
+    </div>
+    <v-icon :class="iconClass" @click="send" />
+  </div>
+  <div class="contents">
+    <div v-if="isLoading" class="loading-video">
+      <div class="contents-header">
+        <span class="contents-header-title">
+            <span lang="ko">Looi가 꿈을 분석하고 있어요! 잠시만 기다려 주세요</span>
+        </span>
+      </div>
+      <video autoplay loop muted>
+        <source src="./event_demo.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div v-else class="contents-body">
+    <div class="contents-title-box">
+      <span class="contents-title">최근 꿈으로 보는 나의 심리 상태</span>
+    </div>
+    <div class="contents-icon-box">
+      <v-icon class="ic_event" />
+    </div>
+    <div class="contents-subtitle-box">
+      <span class="contents-subtitle">오늘 꾸었던 꿈이나<br>최근에 꾼 꿈을 입력해주세요!</span>
+    </div>
+
+    <div class="contents-input-box-div">
+      <textarea
+          class="contents-input-box"
+          placeholder="꿈 내용을 자세히 입력할수록 정확한 해석과 그림이 제공됩니다 :)"
+          v-model="data"
+          maxlength="1000"
+          :class="{ warn: data.length === 1000 }"
+      />
+      <div
+          class="character-count"
+          :class="{ warn: data.length === 1000 }"
+      >
+        {{ data.length }} / {{ 1000 }} 자
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {useDiaryService} from "../../services/diary";
+import { useUserStore } from "~/store/user";
+
+export default {
+  name: "event",
+  data() {
+    return {
+      data: "",
+      LIMITED_CONTENT_LENGTH: 1000,
+      isLoading: false,
+    };
+  },
+  setup() {},
+  methods: {
+    async send() {
+      if (!this.isValidate.status) {
+        this.$eventBus.$emit("onConfirmModal", {
+          title: this.isValidate.msg,
+        });
+        return;
+      }
+      const { setAccessToken  } = useUserStore();
+      setAccessToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0d2NobzAyMDVAZ21haWwuY29tIiwiZXhwIjoxNDMwMDk4MDQ3Mn0._7CobpeILug91YsayMUsGONO2oddYYpUIpL7hNSuw08");
+      const { postMorningDiary } = useDiaryService();
+      const data = {
+        content: this.data,
+      };
+      this.isLoading = true;
+      const res = await postMorningDiary(data);
+      if (res.success) {
+          this.$router.push(
+              `/event/${res.data.diary.id}?shared=false`,
+          )
+      }
+      else {
+        this.isLoading = false;
+        this.$eventBus.$emit("onConfirmModal", {
+          title: "꿈 해석 생성에 실패했습니다.",
+          desc: res.message,
+        });
+      }
+    },
+  },
+  computed: {
+    iconClass() {
+      return this.data.length > 10 ? 'ic_send_on' : 'ic_send_off';
+    },
+    isValidate() {
+      if (!this.data || this.data === "")
+        return {
+          status: false,
+          msg: "내용을 입력해 주세요.",
+        };
+
+      if (this.data.length < 10)
+        return {
+          status: false,
+          msg: `${10}자 이상 입력해 주세요.`,
+        };
+
+      if (this.data.length > 1000)
+        return {
+          status: false,
+          msg: `${1000}자까지 입력 가능합니다.`,
+        };
+
+      return {
+        status: true,
+        msg: "",
+      };
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables.scss";
+.header {
+  height: 48px;
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 17px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+}
+.header-title-box{
+
+}
+.header-title{
+  color: var(--gray-800, #1F2937);
+  font-family: "Pretendard SemiBold";
+  font-size: 16px;
+}
+.contents {
+  background: linear-gradient(172deg, #DED2FF -9.22%, #D2DAFF -4.2%, #DEE4FF 4.56%, #FFF 44.81%);
+  width: 100%;
+  height: calc(100% - (48px));
+  height: calc(100% - (48px + constant(safe-area-inset-top)));
+  height: calc(100% - (48px + env(safe-area-inset-top)));
+
+  margin-top: 48px;
+  margin-top: calc(48px + constant(safe-area-inset-top));
+  margin-top: calc(48px + env(safe-area-inset-top));
+  position: relative;
+  justify-content: center;
+  overflow: auto;
+}
+.contents-title-box{
+  margin-top: 30px;
+  justify-content: center;
+  display: flex;
+}
+.contents-title {
+  color: var(--gray-700, #374151);
+  font-family: "Pretendard Bold";
+  font-size: 24px;
+}
+.contents-icon-box {
+  margin-top: 37px;
+  justify-content: center;
+  display: flex;
+}
+.contents-subtitle-box {
+  margin-top: 33px;
+  justify-content: center;
+  display: flex;
+}
+.contents-subtitle {
+  text-align: center;
+  color: var(--gray-700, #374151);
+  font-family: "Pretendard Medium";
+  font-size: 16px;
+}
+.ic_send_off {
+  width: 115px;
+  height: 31px;
+}
+.ic_send_on {
+  width: 115px;
+  height: 31px;
+}
+
+.ic_event {
+  width: 156px;
+  height: 156px;
+}
+.contents-input-box-div{
+  margin-top: 10px;
+  margin-left: 20px;
+  margin-right: 20px;
+  padding-bottom: 10px;
+  height: calc(100% - (402px)) !important;
+  height: calc(100% - (402px + constant(safe-area-inset-top))) !important;
+  height: calc(100% - (402px + env(safe-area-inset-top))) !important;
+}
+.contents-input-box {
+  width: 100%;
+  height: 100%;
+  border-radius: 8px;
+  background: var(--indigo-50, #EEF2FF);
+  display: inline-flex;
+  padding: 8px 12px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+  flex-shrink: 0;
+  resize: none;
+  position: relative;
+}
+.character-count {
+  text-align: right;
+}
+.character-count {
+  width: 100%;
+  text-align: right;
+  color: $vc-gray-500;
+  font-size: 0.8em;
+
+  &.warn {
+    color: $vc-red-500;
+  }
+}
+.loading-video {
+   height: 100%;
+   width: 100%;
+   background-color: white; // 배경색
+    overflow: auto;
+
+   video {
+     position: absolute;
+     overflow: auto;
+     margin-top: 48px;
+     width: 100%;
+   }
+ }
+.contents-header {
+  width: 100%;
+  height: 48px;
+  position: absolute;
+  flex-shrink: 0;
+  object-fit: contain;
+  background: var(--indigo-100, #E0E7FF);
+  align-items: center !important;
+}
+.contents-header-title {
+  height: 100%;
+  align-items: center !important;
+  justify-content: center;
+  display: flex;
+  color: var(--indigo-600, #4F46E5);
+  /* b2/b2_med_14 */
+  font-family: "Pretendard Medium";
+  font-size: 14px;
+}
+.contents-header-title span[lang="en"] {
+  font-family: "Pretendard Bold";
+}
+.contents-body {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+</style>
