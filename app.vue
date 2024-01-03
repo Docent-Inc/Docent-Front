cd99217ee857cf950ff248556a0ec0ff79c80550
taewan <taewan2002@naver.com>
2024-01-03 18:10:08 +0900

    fix(develop): gtm 설치

--- app.vue
+++ app.vue
@@ -43,7 +43,7 @@ onMounted(() => {
         y.parentNode.insertBefore(t, y);
     })(window, document, "clarity", "script", "jsur0l64cq");
 
-    <!-- Google Tag Manager -->
+    //  Google Tag Manager
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
